dcb4ce2602c17faaebc587324480bd84d8399beb
kylec <krclark8485@hotmail.com>
2024-06-02 14:37:33 -0700

    added inactive game list

---
+++ src/models/Game.ts
@@ -0,0 +1,8 @@
+export module Game {
+    export interface IndexRecord {
+        active: boolean 
+        id: number 
+        team1_name: string | null 
+        team2_name: string | null 
+    }
+}
--- src/screens/GamePicker.tsx
+++ src/screens/GamePicker.tsx
@@ -5,6 +5,9 @@ import axios from 'axios'
 import LoadingScreen from '../components/LoadingScreen'
 import { TeamColors } from '../team/TeamColors'
 import { useNavigate } from 'react-router-dom'
+import { Game } from '../models/Game'
+
+
 
 export default function GamePicker() {
   const navigate = useNavigate()
@@ -22,9 +25,21 @@ export default function GamePicker() {
     queryFn: getGames,
   })
 
-  if (existingGamesQuery.isLoading) {
+  if (existingGamesQuery.isLoading || !existingGamesQuery.data) {
     return <LoadingScreen />
   }
+
+  const activeGames: Game.IndexRecord[] = []
+  const inactiveGames: Game.IndexRecord[] = []
+
+  existingGamesQuery.data.forEach((game: Game.IndexRecord) => {
+    if (game.active) {
+      activeGames.push(game)
+    } else {
+      inactiveGames.push(game)
+    }
+  })
+
 
   if (existingGamesQuery.error || !existingGamesQuery.data.length) {
     return (
@@ -57,15 +72,11 @@ export default function GamePicker() {
     <Box p={3}>
       <Typography variant="h2" fontWeight={500} mb={4}>
         {' '}
-        Find an Existing Game{' '}
+        Find an Active Game{' '}
       </Typography>
 
-      {existingGamesQuery.data.map(
-        (game: {
-          id: number
-          team1_name: string | null
-          team2_name: string | null
-        }) => {
+      {activeGames.map(
+        (game) => {
           return (
             <Box key={game.id}>
               Game ID: {game.id}
@@ -118,6 +129,53 @@ export default function GamePicker() {
             </Box>
           )
         }
+      )}
+
+      {inactiveGames.length && (
+        <Box mt={3}>
+          <Typography variant="h2" fontWeight={500} mb={4}>
+            Finished Games
+          </Typography>
+
+          {inactiveGames.map((game) => {
+            return (
+              <Box key={game.id}>
+              Game ID: {game.id}
+              <Box
+                display="flex"
+                justifyContent="center"
+                mb={4}
+                maxWidth={300}
+                alignItems="center"
+                style={{ cursor: 'pointer' }}
+                sx={{
+                  transition: 'all 0.25s ease',
+                  '&:hover': {
+                    boxShadow: '10px 5px 5px rgba(0, 0, 0, 0.20)',
+                  },
+                }}
+                borderRadius={2}
+                p={1}
+                border={'1px solid rgba(0, 0, 0, 0.2)'}
+              >
+                <Box>
+                  <img
+                    style={{ maxWidth: 100 }}
+                    src={TeamColors[game.team1_name as any].png}
+                  />
+                </Box>
+                <Typography mx={2}>VS</Typography>
+                <Box>
+                  <img
+                    style={{ maxWidth: 100 }}
+                    src={TeamColors[game.team2_name as any].png}
+                  />
+                </Box>
+              </Box>
+            </Box>
+            )
+          })}
+        </Box>
       )}
     </Box>
   )
